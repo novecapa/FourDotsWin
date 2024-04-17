40318013e4f072e5ead6092106dbe0511171ef65
Josep Cerdá Penadés <josep@daapcoders.com>
2024-04-17 22:57:51 +0200

    Initial commit

---
+++ ThreeDotsWin/ThreeDotsWinApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreeDotsWinApp.swift
+//  ThreeDotsWin
+//
+//  Created by Josep Cerdá Penadés on 17/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ThreeDotsWinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
